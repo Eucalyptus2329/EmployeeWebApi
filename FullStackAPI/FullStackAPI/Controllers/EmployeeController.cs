90af351c23c427563a296aa9b826794e8659f131
Eulix Chiu <eulix.chiu@gmail.com>
2022-12-28 14:04:19 -0800

    Delete Changes

--- FullStackAPI/FullStackAPI/Controllers/EmployeeController.cs
+++ FullStackAPI/FullStackAPI/Controllers/EmployeeController.cs
@@ -61,5 +61,24 @@ namespace FullStackAPI.Controllers
             await _fullStackDbContext.SaveChangesAsync();
             return Ok(employee);
         }
+        [HttpDelete]
+        [Route("{id:Guid}")]
+
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await _fullStackDbContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _fullStackDbContext.Employees.Remove(employee);
+            await _fullStackDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+
     }
+}
 }
